9f9af4821aa415b916853570a95f6b0cfbab9e79
1-ken <kennicmwimu@gmail.com>
2025-09-18 20:59:42 +0300

    basic functionality

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,70 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page class="flex flex-center text-white">
+    <div class="row">
+      <q-input
+      v-model="data.name"
+      placeholder="Counter"
+      filled
+      color="teal"
+      input-class="text-center text-white text-h5"
+      />
+    </div>
+    <div class="row full-width">
+      <div class="col text-center">
+        <q-btn
+        @click="decreaseCounter"
+        icon="remove"
+        size="xl"
+        round
+        />
+      </div>
+      <div class="col text-center text-h2">
+        {{data.couter}}
+      </div>
+      <div class="col text-center">
+        <q-btn
+        @click="increaseCounter"
+        icon="add"
+        size="xl"
+        round
+        />
+      </div>
+    </div>
+    <div class="row">
+       <q-btn
+       @click="resetCounter"
+        icon="restart_alt"
+        size="xl"
+        round
+        />
+    </div>
   </q-page>
 </template>
+<style scoped>
+.q-page {
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
 
 <script setup>
-//
+// imports
+import { reactive } from 'vue';
+// data
+const data = reactive({
+  couter:0,
+  name:''
+})
+// counter methods
+const increaseCounter = () =>{
+  data.couter ++
+}
+const decreaseCounter = () =>{
+  if(data.couter > 0){
+    data.couter --
+  }
+}
+ const resetCounter = () =>{
+    data.couter = 0
+  }
 </script>
